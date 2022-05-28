d0c578c7e8a7265ffd8655938723b782796a6660
Max Baumann <max@bmn.dev>
2022-05-28 14:38:06 +0200

    Update browser.ts

--- src/browser.ts
+++ src/browser.ts
@@ -4,7 +4,7 @@ const TWITCH_PLAYSTORE_URL =
   'https://play.google.com/store/apps/details?id=tv.twitch.android.app&hl=en_US&gl=US';
 
 export async function getLatestUpdateDate(): Promise<string | undefined> {
-  const browser = await puppetteer.launch({headless: false});
+  const browser = await puppetteer.launch();
   const page = await browser.newPage();
   await page.goto(TWITCH_PLAYSTORE_URL);
 
